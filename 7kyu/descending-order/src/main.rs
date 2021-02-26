336ed635f48810d6e020cb8e647cc9005bf88769
kenesparta <kenesparta@pm.me>
2021-02-25 20:15:33 -0500

    adding descending order

---
+++ 7kyu/descending-order/src/main.rs
@@ -0,0 +1,13 @@
+fn descending_order(x: u64) -> u64 {
+    unimplemented!()
+}
+
+fn main() {
+    assert_eq!(descending_order(0), 0);
+    assert_eq!(descending_order(1), 1);
+    assert_eq!(descending_order(15), 51);
+    assert_eq!(descending_order(1021), 2110);
+    assert_eq!(descending_order(123456789), 987654321);
+    assert_eq!(descending_order(145263), 654321);
+    assert_eq!(descending_order(1254859723), 9875543221);
+}
