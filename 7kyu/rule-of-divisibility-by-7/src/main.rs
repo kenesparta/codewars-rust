26b289757cc1f6d29cd445493d22d2fa744697d4
kenesparta <kenesparta@pm.me>
2021-05-15 14:31:49 -0500

    solve A Rule of Divisibility by 7

--- 7kyu/rule-of-divisibility-by-7/src/main.rs
+++ 7kyu/rule-of-divisibility-by-7/src/main.rs
@@ -1,5 +1,15 @@
-fn seven(n: i64) -> (i64, i32) {}
+fn seven(n: i64) -> (i64, i32) {
+    // let div = n % 10;
+    let mut temporal_number = n;
+    let mut number_steps: i32;
+    number_steps = 0;
+    while temporal_number >= 100 {
+        temporal_number = temporal_number / 10 - 2 * (temporal_number % 10);
+        number_steps += 1;
+    }
+    (temporal_number, number_steps)
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", seven(477557101));
 }
